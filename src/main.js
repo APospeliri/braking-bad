da8981c13cc9a1118dc81ad5110f89bc93a17c1c
IPospelova <iri_shka_s@mail.ru>
2020-07-13 21:21:22 +0300

    add - add styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//styles
+import './assets/styles/main.sass'
+
 Vue.config.productionTip = false
 
 new Vue({
