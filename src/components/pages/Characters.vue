2b93c6c7e2e9ee586574a2a1afb29970bc8973d7
IPospelova <iri_shka_s@mail.ru>
2020-07-15 21:23:47 +0300

    add - filter function

--- src/components/pages/Characters.vue
+++ src/components/pages/Characters.vue
@@ -6,6 +6,9 @@
         <sidebar></sidebar>
       </div>
       <div class="col-md-9">
+        <button class="btn btn--sidebar" @click="filterByAlive()">
+          filter by alive
+        </button>
         <h1 class="text text--h1">Characters</h1>
         <gallery :characters="characters"></gallery>
       </div>
@@ -318,43 +321,13 @@ export default {
           better_call_saul_appearance: []
         }
       ]
-      // characters: [
-      //   {
-      //     imgSrc:
-      //       'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-      //     nick: 'Heisenberg1',
-      //     name: 'Walter White1',
-      //     birthday: '09-07-1958'
-      //   },
-      //   {
-      //     imgSrc:
-      //       'https://majdkhatib.com/wp-content/uploads/2018/03/Breaking-Bad-Walter-White1.jpg?x72778',
-      //     nick: 'Heisenberg2',
-      //     name: 'Walter White2',
-      //     birthday: '09-07-1958'
-      //   },
-      //   {
-      //     imgSrc:
-      //       'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-      //     nick: 'Heisenberg3',
-      //     name: 'Walter White3',
-      //     birthday: '09-07-1958'
-      //   },
-      //   {
-      //     imgSrc:
-      //       'https://majdkhatib.com/wp-content/uploads/2018/03/Breaking-Bad-Walter-White1.jpg?x72778',
-      //     nick: 'Heisenberg4',
-      //     name: 'Walter White4',
-      //     birthday: '09-07-1958'
-      //   },
-      //   {
-      //     imgSrc:
-      //       'https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg',
-      //     nick: 'Heisenberg5',
-      //     name: 'Walter White5',
-      //     birthday: '09-07-1958'
-      //   }
-      // ]
+    }
+  },
+
+  methods: {
+    filterByAlive() {
+      this.characters = this.characters.filter(el => el.status === 'Alive')
+      return this.characters
     }
   }
 }
