8423972003511c04c0ff45e144cb628d6e01c133
IPospelova <iri_shka_s@mail.ru>
2020-07-15 21:14:36 +0300

    fix - data in characters page

--- src/components/blocks/Gallery.vue
+++ src/components/blocks/Gallery.vue
@@ -16,9 +16,9 @@
 
           <!-- if charcters -->
           <div v-if="$route.path === '/'" class="gallery__bottom">
-            <h3 class="gallery__nick">nick "{{ character.nick }}"</h3>
-            <p class="gallery__name">name "{{ character.name }}"</p>
-            <p class="gallery__date">birthday: "{{ character.birthday }}"</p>
+            <h3 class="gallery__nick">nick: {{ character.nickname }}</h3>
+            <p class="gallery__name">name: {{ character.name }}</p>
+            <p class="gallery__date">birthday: {{ character.birthday }}</p>
           </div>
 
           <!-- else seasons -->
