9f46f69f795188bd25fb46e4f01165c6cdb91e17
IPospelova <iri_shka_s@mail.ru>
2020-07-15 20:54:16 +0300

    fix - seasons img link

--- src/components/pages/Seasons.vue
+++ src/components/pages/Seasons.vue
@@ -45,7 +45,7 @@ export default {
           ],
           episode: '1',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BNTZlMGY1OWItZWJiMy00MTZlLThhMGItNDQ2ODM3YzNkOWU5XkEyXkFqcGdeQXVyNzgyOTQ4MDc@._V1_UY126_UX224_AL_.jpg'
         },
         {
@@ -62,7 +62,7 @@ export default {
           ],
           episode: '2',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BNmI5MTU3OTAtYTczMC00MDE5LTg3YjMtMjA3NWEyMmYyZWQwXkEyXkFqcGdeQXVyNjk1MzkzMzM@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -82,7 +82,7 @@ export default {
           ],
           episode: '3',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BMjM0Mjc0NjI0NV5BMl5BanBnXkFtZTgwMDg2MDU5NTM@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -101,7 +101,7 @@ export default {
           ],
           episode: '4',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BMjM0Mjc0NjI0NV5BMl5BanBnXkFtZTgwMDg2MDU5NTM@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -122,7 +122,7 @@ export default {
           ],
           episode: '5',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BNTgwOTE0ODYtMDEwNC00MjY1LWJjZDctNTU2MjRlNTgzY2NkXkEyXkFqcGdeQXVyNjg0Nzk2Nzc@._V1_UY126_UX224_AL_.jpg'
         },
         {
@@ -141,7 +141,7 @@ export default {
           ],
           episode: '6',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BOGU0YTA2ZGItNDFjNi00NWM1LTk5NzAtYjFkYTk2MzFhY2YyXkEyXkFqcGdeQXVyNjc0NTIwNTU@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -160,7 +160,7 @@ export default {
           ],
           episode: '7',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BNzM1OTA1NzgxOV5BMl5BanBnXkFtZTgwMjg2MDU5NTM@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -179,7 +179,7 @@ export default {
           ],
           episode: '1',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BMjEwNjk4MTI2MF5BMl5BanBnXkFtZTcwMTIzMTMzMg@@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -199,7 +199,7 @@ export default {
           ],
           episode: '2',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BZjAyYmJlMWQtYjg0Zi00NjBiLTg1ZGUtMDRiZjcwMjY1NzM2XkEyXkFqcGdeQXVyNjg0Nzk2Nzc@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -218,7 +218,7 @@ export default {
           ],
           episode: '3',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BNzA5OWQxM2MtNzI0ZS00NWI4LWJmYWEtNjA3ODJmMzUzMzFmXkEyXkFqcGdeQXVyNjE0NDg1MDg@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -234,7 +234,7 @@ export default {
           ],
           episode: '4',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BMjI0NzA3MDc0MF5BMl5BanBnXkFtZTgwMTE4OTc5MjE@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -254,7 +254,7 @@ export default {
           ],
           episode: '5',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BMjIwOTg0MDU2MV5BMl5BanBnXkFtZTcwNjE2NTc0Mg@@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -272,7 +272,7 @@ export default {
           ],
           episode: '6',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BMTk0NjI4NTYwOV5BMl5BanBnXkFtZTgwOTc2MDU5NTM@._V1_UX224_CR0,0,224,126_AL_.jpg'
         },
         {
@@ -293,7 +293,7 @@ export default {
           ],
           episode: '7',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BYTBkYjQxOTQtOWNlNC00YmMxLWJlY2EtN2ZhNDBkYjc5OWU3XkEyXkFqcGdeQXVyNjE0NDg1MDg@._V1_UY126_CR1,0,224,126_AL_.jpg'
         },
         {
@@ -313,7 +313,7 @@ export default {
           ],
           episode: '8',
           series: 'Breaking Bad',
-          img:
+          imgSrc:
             'https://m.media-amazon.com/images/M/MV5BMTk1MTI3NjY1Ml5BMl5BanBnXkFtZTgwMTk2MDU5NTM@._V1_UX224_CR0,0,224,126_AL_.jpg'
         }
       ]
